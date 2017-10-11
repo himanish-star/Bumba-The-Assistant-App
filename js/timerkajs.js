33b6aaa1368cef22e7bddcaf5c12d30e1946818d
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2017-10-11 20:29:01 +0530

    still stuck in the errorgit add .git add . DAMN

--- js/timerkajs.js
+++ js/timerkajs.js
@@ -1,68 +1,71 @@
 // let countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
-let distance = 2700000;
-let k2=0;
+window.onload=()=> {
+
+    let distance = 2700000;
+    let minutes, seconds;
+    let k2 = 0;
+    let clock = document.getElementById("clk-btn");
+    document.getElementById("clk-btn").setAttribute('data-content', '6');
 // Update the count down every 1 second
-let x = setInterval(function() {
+    let x = setInterval(function () {
 
-    // Get todays date and time
-    // let now = new Date().getTime();
+        // Get todays date and time
+        // let now = new Date().getTime();
 
-    // Find the distance between now an the count down date
-    distance = distance -1000;
-
-
-    // Time calculations for days, hours, minutes and seconds
-    // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    // let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = "<i class=\"fa fa-clock-o btn btn-primary\"></i>     " +
-    minutes + "m " + seconds + "s ";
-    // If the count down is finished, write some text
-    if (distance < 0) {
-
-        document.getElementById("demo").innerHTML = "go take a walk";
-        setTimeout(function(){
-            document.getElementById("demo").innerHTML = "starting again in a while";
-            distance = 2700000},5000);
-    }
-}, 1000);   
+        // Find the distance between now an the count down date
+        distance = distance - 1000;
 
 
+        // Time calculations for days, hours, minutes and seconds
+        // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        // let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-let k=1;
-let clock = document.getElementById("clk-btn");
-timer.setAttribute('data-content',k2);
-clock.onclick = function () {
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = minutes + " " + seconds;
+        k2 = minutes;
+        document.getElementById("clk-btn").setAttribute('data-content', k2.toString());
+        // If the count down is finished, write some text
+        if (distance < 0) {
 
-    // $("#demo").attr("display","block");
-    let timer = document.getElementById("demo");
-    if(k%2)
-        timer.style.display = 'block';
-    else
-        timer.style.display = 'none';
-    k++;
-    // console.log(k);
+            document.getElementById("demo").innerHTML = "go take a walk";
+            setTimeout(function () {
+                document.getElementById("demo").innerHTML = "starting again in a while";
+                distance = 2700000
+            }, 5000);
+        }
+    }, 1000);
 
+    let k = 1;
+    clock.onclick = function () {
+
+        // $("#demo").attr("display","block");
+        let timer = document.getElementById("demo");
+        if (k % 2)
+            timer.style.display = 'block';
+        else
+            timer.style.display = 'none';
+        k++;
+        // console.log(k);
+
+    };
+    let read_more = document.getElementById("read_more");
+    let t = 1;
+    read_more.onclick = function () {
+
+        // $("#demo").attr("display","block");
+        read_more.style.display = 'none';
+        let more_text = document.getElementById("more_text");
+        if (t % 2)
+            more_text.style.display = 'block';
+        else
+            more_text.style.display = 'none';
+        t++;
+        // console.log(k);
+
+    };
 };
-let read_more= document.getElementById("read_more");
-let t=1;
-read_more.onclick = function () {
-
-    // $("#demo").attr("display","block");
-    read_more.style.display = 'none';
-    let more_text = document.getElementById("more_text");
-    if(t%2)
-        more_text.style.display = 'block';
-    else
-        more_text.style.display = 'none';
-    t++;
-    // console.log(k);
-
-};
-
 
 
 
