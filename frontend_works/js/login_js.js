8acf98bc24ef26e6d4218d30a377626680f5a7a4
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2017-12-04 22:01:12 +0530

    setting up the new look of the frontend

--- frontend_works/js/login_js.js
+++ frontend_works/js/login_js.js
@@ -1,15 +1,1 @@
-/*
-const passport = require('passport');
 
-$(function () {
-
-        $.get('auth/google/', ( passport.authenticate('google',
-                {
-                    scope: ['profile']
-                } )
-            )
-        );
-    }
-)
-
-*/
