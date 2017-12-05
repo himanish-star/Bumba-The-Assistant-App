508dacb643ae1052e46a071521fb64dfef0975d2
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2017-12-05 22:57:38 +0530

    first slide of design

---
+++ frontend_works/js/for_tabs.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.tabs .tab-links a').on('click', function(e)  {
+        let currentAttrValue = $(this).attr('href');
+        e.preventDefault();
+        // Show/Hide Tabs
+        let alpha = '.tabs ' + currentAttrValue;
+        $(alpha).slideDown(400).siblings().slideUp(400);
+
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+
+
+    });
+});
+
+// Show/Hide Tabs
