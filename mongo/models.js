38e140605de56e168a2f4499152ce17b9aecfc5b
shikhar-scs <shikhar9a@gmail.com>
2018-01-11 16:40:37 +0530

    hey

--- mongo/models.js
+++ mongo/models.js
@@ -5,7 +5,7 @@ let ObjectId = require('mongodb').ObjectId;
 
 let users,categories,urls,todos, typeOfCanvas = null;
 
-MongoClient.connect(DataBase.URI, function (err,db) {
+MongoClient.connect(DataBase.mlabURI, function (err,db) {
     if(err) throw err;
     console.log("connected");
     categories = db.collection('categories');
